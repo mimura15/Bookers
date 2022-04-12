148a7c82d19dc215052fecd7284deafc77cac9e8
mimura15 <102932331+mimura15@users.noreply.github.com>
2022-04-12 13:03:21 +0000

    [Add] edit編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
   get 'books' => 'books#index'
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show', as: 'book'
-  get 'books/edit'
-  root to: 'homes#top'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
