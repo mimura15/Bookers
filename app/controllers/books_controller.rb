2b65db5f121cea9a2842c961f49bba7336fda348
mimura15 <102932331+mimura15@users.noreply.github.com>
2022-04-13 13:30:33 +0000

    [Add] createフラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,12 @@ class BooksController < ApplicationController
   
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      flash[:notice] = "Book was successfully created."  
+      redirect_to book_path(book.id)
+    else
+      render action: :new
+    end
   end
 
   def show
